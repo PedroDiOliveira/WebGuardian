bb286d1be57ceca3e0004af54ed41336063691c1
PedroDiOliveira <pedro.h.soterio@gmail.com>
2024-05-16 17:57:58 -0300

    feat: Creates server inicial structure

---
+++ go-server/go.mod
@@ -0,0 +1,3 @@
+module web-guardian
+
+go 1.22.3
---
+++ go-server/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type Router struct{}
+
+func (Router) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	res.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+	res.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+}
+
+func RunServer() {
+	s := http.Server{
+		Addr:         ":8080",
+		Handler:      Router{},
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+	}
+
+	log.Fatal(s.ListenAndServe())
+}

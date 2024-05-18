e0e6462739abe37d46a70fea85929d28cf575ce9
PedroDiOliveira <pedro.h.soterio@gmail.com>
2024-05-17 22:38:54 -0300

    starting front-end

--- web-guardian/src/router/index.js
+++ web-guardian/src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from "../view/Home.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path:"/",
+      component:Home
+    }
   ]
 })
 
--- web-guardian/src/view/Home.vue
+++ web-guardian/src/view/Home.vue
@@ -1,10 +1,62 @@
 <script>
-    
+
 </script>
 
-<template>
-
+<template >
+    <div class="home">
+        <div class="titulo">
+            <h1>Bem vindo ao WebGuadian</h1>
+        </div>
+        <div class="container">
+            <p>Iniciar monitoramento</p>
+            <RouterLink ><button class="botao"></button></RouterLink>
+        </div>
+    </div>
 </template>
 
 <style>
+    .home{
+        background-image: url(../assets/image/background.jpg);
+        background-size: cover;
+        background-position: center;
+        height: 100%;
+        padding-block: 10%;
+
+    }
+    
+    h1{
+        font-family: "Rubik", sans-serif;
+        color: white;
+    }
+
+    p{
+        font-family: "Rubik", sans-serif;
+        color: white;
+        padding: 4%;
+    }
+    
+    .botao{
+        height: 5em;
+        width: 5em;
+        background-color:white;
+        border-radius: 50%;
+        border: 6px white gray;
+    }
+
+    .titulo{
+        height: 50%;
+        
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .container{
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: end;
+    }
 </style>

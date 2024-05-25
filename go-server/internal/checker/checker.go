80e7aca8032328d06bd75996edb046531a27a8ff
PedroDiOliveira <pedro.h.soterio@gmail.com>
2024-05-25 00:45:43 -0300

    Fear: Implements new response coder for HTTP response

--- go-server/internal/checker/checker.go
+++ go-server/internal/checker/checker.go
@@ -7,15 +7,32 @@ type HTTPMessage struct {
 	Description string `json:"desc"`
 }
 
-var messages = map[int]HTTPMessage{
-	404: {Code: 404, Description: "Not found"},
-	200: {Code: 200, Description: "OK"},
+type errorMessage struct {
+	Err string `json:"err"`
 }
 
-func Check(url string) HTTPMessage {
+var mensagem errorMessage = 
+
+mensagem.Err = "Invalid URL"
+
+var messages = map[int]HTTPMessage{
+	404: {Code: 404, Description: "Not found"},
+	200: {Code: 200, Description: "Online Server"},
+	429: {Code: 429, Description: "Too many requests"},
+	408: {Code: 408, Description: "Request timeout"},
+	500: {Code: 500, Description: "Internal Server Error"},
+	502: {Code: 502, Description: "Bad gateway"},
+	503: {Code: 503, Description: "Service unavailable"},
+}
+
+func Check(url string) (HTTPMessage, errorMessage) {
 	resp, _ := http.Get(url)
 
 	message := messages[resp.StatusCode]
 
-	return message
+	if message != nil {
+		return message, nil
+	}
+
+	return _, 
 }
